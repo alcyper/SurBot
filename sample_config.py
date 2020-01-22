9438ce0d9a7bb77f4d6419648a664846be91a79f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-01-22 19:38:47 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -73,9 +73,9 @@ class Config(object):
         "welcome",
         "markdown",
         "mention",
+   
+
     ]
-
-
     # Get your own API key from https://www.remove.bg/ or
     # feel free to use http://telegram.dog/Remove_BGBot
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
