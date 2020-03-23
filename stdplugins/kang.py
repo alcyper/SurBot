68186421bace490ad53ffdf02d292add06a89c87
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-03-23 15:47:02 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"✰Sᴀͥʀᴀͣᴛͫʜ™️✰ @Sur_vivor"
-    packshortname = f"Sarath-Survivor{userid}"  # format: Uni_Borg_userid
+    packshortname = f"Sarath_Survivor{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "✰Sᴀͥʀᴀͣᴛͫʜ™️✰_Sticker.png"
@@ -54,8 +54,8 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"✰Sᴀͥʀᴀͣᴛͫʜ™️✰-AnimatedStickers"
-        packshortname = f"Survivor-Animated"  # format: Uni_Borg_userid
+        packname = f"✰Sᴀͥʀᴀͣᴛͫʜ™️✰_AnimatedStickers"
+        packshortname = f"Survivor_Animated"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
