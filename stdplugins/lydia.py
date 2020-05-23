93b4c9badb5486d6e6384d246b92d17987f4e668
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-23 12:58:31 +0530

    Add files via upload

--- stdplugins/lydia.py
+++ stdplugins/lydia.py
@@ -7,15 +7,11 @@
 Description: A module that Act as a chatbot and chat with a User/other Bot.
 This Module Needs CoffeeHouse API to work. so Join https://telegram.dog/IntellivoidDev and send #activateapi and follow instructions.
 This Module also Needs DB_URI For Storage of Some Data So make sure you have that too.
-
-Credits:
-@Hackintosh5 (for inspiring me to write this module)
-@Zero_cool7870 (For Writing The Original Module)
-Zi Xing (For CoffeeHouse API)"""
+"""
 
 
-import coffeehouse as cf
-
+from coffeehouse.lydia import LydiaAI
+from coffeehouse.api import API
 import asyncio
 import io
 from sql_helpers.lydia_ai_sql import get_s, get_all_s, add_s, remove_s
@@ -24,8 +20,10 @@ from uniborg.util import admin_cmd
 
 if Config.LYDIA_API is not None:
     api_key = Config.LYDIA_API
-    # Initialise client
-    api_client = cf.API(api_key)
+    # Create the coffeehouse API
+    coffeehouse_api = API(api_key)
+    # Create Lydia instance
+    lydia = LydiaAI(coffeehouse_api)
 
 
 @borg.on(admin_cmd(pattern="(e|d|l)ai", allow_sudo=True))
@@ -40,15 +38,20 @@ async def lydia_disable_enable(event):
         reply_msg = await event.get_reply_message()
         user_id = reply_msg.from_id
         chat_id = event.chat_id
-        await event.edit("hmm")
+        await event.edit("hoi")
         if input_str == "e":
-            session = api_client.create_session()
+            # Create a new chat session (Like a conversation)
+            session = lydia.create_session()
             logger.info(session)
+            # logger.info("Session ID: {0}".format(session.id))
+            # logger.info("Session Available: {0}".format(str(session.available)))
+            # logger.info("Session Language: {0}".format(str(session.language)))
+            # logger.info("Session Expires: {0}".format(str(session.expires)))
             logger.info(add_s(user_id, chat_id, session.id, session.expires))
-            await event.edit(f"[Hi ☺️ , How are you](tg://user?id={user_id})")
+            await event.edit(f"hi")
         elif input_str == "d":
             logger.info(remove_s(user_id, chat_id))
-            await event.edit(f"[🥺 Bye , have to go...](tg://user?id={user_id})")
+            await event.edit(f"[__signal lost__](tg://user?id={user_id})")
         elif input_str == "l":
             lsts = get_all_s()
             if len(lsts) > 0:
@@ -60,7 +63,7 @@ async def lydia_disable_enable(event):
             if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
                 with io.BytesIO(str.encode(output_str)) as out_file:
                     out_file.name = "lydia_ai.text"
-                    await borg.send_file(
+                    await event.client.send_file(
                         event.chat_id,
                         out_file,
                         force_document=True,
@@ -82,6 +85,13 @@ async def on_new_message(event):
         return
     if Config.LYDIA_API is None:
         return
+    reply = await event.get_reply_message()
+    if reply is None:
+        pass
+    elif reply.from_id == borg.uid:
+        pass
+    else:
+        return
     if not event.media:
         user_id = event.from_id
         chat_id = event.chat_id
@@ -95,16 +105,16 @@ async def on_new_message(event):
             # then there's an issue with the API, Auth or Server.
             if session_expires < time():
                 # re-generate session
-                session = api_client.create_session()
+                session = lydia.create_session()
                 logger.info(session)
                 session_id = session.id
                 session_expires = session.expires
                 logger.info(add_s(user_id, chat_id, session_id, session_expires))
             # Try to think a thought.
             try:
-                async with event.client.action(event.chat_id, "typing"):
-                    await asyncio.sleep(1)
-                    output = api_client.think_thought(session_id, query)
-                    await event.reply(output)
+                async with event.client.action(event.chat_id, "location"):
+                    await asyncio.sleep(5)
+                    output = lydia.think_thought(session_id, query)
+                    await event.reply("💫"+output)
             except cf.exception.CoffeeHouseError as e:
                 logger.info(str(e))
